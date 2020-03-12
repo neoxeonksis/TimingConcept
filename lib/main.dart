3cd6be60617c820faf891393ecd65a42ce5d2133
jidrone <jidrone20@gmail.com>
2020-03-11 22:31:31 -0300

    Added forced portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:timing_ready/src/pages/home_page.dart';
 import 'package:timing_ready/src/pages/evento_page.dart';
@@ -11,6 +12,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       title: 'TimingReady',
       debugShowCheckedModeBanner: false,
