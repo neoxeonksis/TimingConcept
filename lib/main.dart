cda7ef6edafa9317b9277018af5b8841c076ac08
Jorge <47952708+J4ackSk3le7on@users.noreply.github.com>
2020-04-29 12:34:46 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp
-    ]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       title: 'TimingReady',
       debugShowCheckedModeBanner: false,
